5cf1fd75e3331a0d581ac7123cfa58bd14ee9414
wynkth283 <wynkth283@gmail.com>
2025-07-03 23:56:32 +0700

    Thêm dữ liệu seed LoaiQuyen

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Seed LoaiQuyen
+loai_quyen_admin = LoaiQuyen.find_or_create_by!(TenQuyen: 'Admin')
+loai_quyen_khach = LoaiQuyen.find_or_create_by!(TenQuyen: 'KhachHang')
+loai_quyen_nv = LoaiQuyen.find_or_create_by!(TenQuyen: 'NhanVien')
+
+# Seed Admin user
+Nguoidung.find_or_create_by!(Email: 'admin@quanlybansach.com') do |admin|
+  admin.TenND = 'Admin'
+  admin.AnhDaiDien = '' # Đường dẫn ảnh đại diện nếu có
+  admin.DiaChi = 'Cà Mau'
+  admin.SDT = 123456789
+  admin.MatKhau = 'admin123' # Nên mã hóa mật khẩu nếu có xác thực
+  admin.loai_quyen_id = loai_quyen_admin.id
+end
